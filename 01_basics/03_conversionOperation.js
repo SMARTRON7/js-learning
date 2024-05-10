e9d045f3024ee2acf4754c09d359fb2728753848
SMARTRON7 <36684895+SMARTRON7@users.noreply.github.com>
2024-05-10 21:34:19 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = "35"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+let isLoggedIn = 3
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+let someNum = 34
+
+let stringNum = String(someNum)
+
+console.log(typeof stringNum);
+console.log(stringNum)
