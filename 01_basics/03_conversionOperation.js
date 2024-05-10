52d689f9126a885e24b796151a34239e6ae91b62
SMARTRON7 <36684895+SMARTRON7@users.noreply.github.com>
2024-05-10 21:50:50 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,19 +1,40 @@
 let score = "35"
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
 let isLoggedIn = 3
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn);
-console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
 
 let someNum = 34
 
 let stringNum = String(someNum)
 
-console.log(typeof stringNum);
-console.log(stringNum)
+// console.log(typeof stringNum);
+// console.log(stringNum)
+
+// *********Operations**********
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue);
+
+let str1 = "Hello"
+let str2 = " World"
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(true);
+
