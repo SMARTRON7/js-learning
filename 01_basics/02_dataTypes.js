8eeabcd6b6799bdee3cb51a2412c6f06fca73642
SMARTRON7 <36684895+SMARTRON7@users.noreply.github.com>
2024-05-10 21:17:54 +0000

    Dataypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict"; //Treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+let name  = "Monu"
+let age = 2
+let isLoggedIn = true
+
+console.log(typeof name)
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
