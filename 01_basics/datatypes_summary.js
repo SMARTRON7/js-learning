2c1c4b1479a5e588793c2891265ef5090891a96e
SMARTRON7 <36684895+SMARTRON7@users.noreply.github.com>
2024-05-13 03:23:53 +0000

    Data type summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,22 @@
+// Defining variables
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const pirates = ["Luffy", "Zoro", "Sanji"]
+
+let myObj = {
+    name: "Monu",
+    age: 24,
+
+}
+
+console.log(myObj);
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof myFunction);
